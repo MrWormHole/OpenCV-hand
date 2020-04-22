60243383fed9d9fda30f6ee8bbe9d2e85e33c366
mrwormhole <talhaaltinel@hotmail.com>
2020-04-22 15:46:20 +0100

    visualization of fingertips

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def segment(frame, threshold = 25):
     diff = cv2.absdiff(background.astype("uint8"), frame)
 
     # Apply a threshold to the difference to get the foreground
-    _ , thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)
+    ret, thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)
 
     # Grab the external contours from thresholded foreground
     image, contours, hierarchy = cv2.findContours(thresholded.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -56,7 +56,8 @@ def segment(frame, threshold = 25):
 def count_fingers(thresholded, hand_segment):
     '''
         Given a thresholded image and hand_segment, compute convex hull then find 4 most outward points. Pick a center of these extreme outward points.
-        Generate a circle with 80% radius of max distance.
+        Generate a circle with 80% radius of max distance. 
+        Cut out obtained thresholded using the generated circle.
     '''
     # Compute the convex hull of the hand segment
     conv_hull = cv2.convexHull(hand_segment)
@@ -81,113 +82,83 @@ def count_fingers(thresholded, hand_segment):
     radius = int(0.8 * max_distance)
     circumference = (2 * np.pi * radius)
 
-    # Not grab an ROI of only that circle
+    # Draw the circle into circular ROI
     circular_roi = np.zeros(thresholded.shape[:2], dtype="uint8")
-    
-    # draw the circular ROI
     cv2.circle(circular_roi, (cX, cY), radius, 255, 10)
-    
-    # Using bit-wise AND with the cirle ROI as a mask.
-    # This then returns the cut out obtained using the mask on the thresholded hand image.
+    # Returns the cut out obtained using the mask(circular_roi) on the thresholded hand image.
     circular_roi = cv2.bitwise_and(thresholded, thresholded, mask=circular_roi)
-
-    # Grab contours in circle ROI
+    # Grab external contours in circle ROI
     image, contours, hierarchy = cv2.findContours(circular_roi.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-    # Finger count starts at 0
+    # Fingers counted and collect fingertip countours
     count = 0
-
-    # loop through the contours to see if we count any more fingers.
-    for cnt in contours:
-        
+    fingertip_contours = []
+    for contour in contours:
         # Bounding box of countour
-        (x, y, w, h) = cv2.boundingRect(cnt)
+        (x, y, w, h) = cv2.boundingRect(contour)
 
         # Increment count of fingers based on two conditions:
-        
         # 1. Contour region is not the very bottom of hand area (the wrist)
         out_of_wrist = ((cY + (cY * 0.25)) > (y + h))
-        
         # 2. Number of points along the contour does not exceed 25% of the circumference of the circular ROI (otherwise we're counting points off the hand)
-        limit_points = ((circumference * 0.25) > cnt.shape[0])
-        
-        
+        limit_points = ((circumference * 0.25) > contour.shape[0])
+            
         if  out_of_wrist and limit_points:
             count += 1
-
-    return count
+            fingertip_contours.append(contour + (roi_right, roi_top))
+            
+    return count, fingertip_contours
 
 
 if __name__ == "__main__":
-    
+    # Intialize a video and a frame count
     cam = cv2.VideoCapture(0)
-
-    # Intialize a frame count
     num_frames = 0
 
-    # keep looping, until interrupted
     while True:
-        # get the current frame
+        # Read current frame
         ret, frame = cam.read()
-
-        # flip the frame so that it is not the mirror view
+        # Flip the frame so that it is not the mirror view
         frame = cv2.flip(frame, 1)
-
-        # clone the frame
         frame_copy = frame.copy()
-
-        # Grab the ROI from the frame
+        # Take the ROI from the frame
         roi = frame[roi_top:roi_bottom, roi_right:roi_left]
-
-        # Apply grayscale and blur to ROI
+        # Apply grayscale and blur
         gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
         # For the first 60 frames we will calculate the average of the background.
-        # We will tell the user while this is happening
         if num_frames < 60:
             accumulate(gray)
             if num_frames <= 59:
-                cv2.putText(frame_copy, "WAIT! GETTING BACKGROUND AVG.", (200, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
-                cv2.imshow("Finger Count",frame_copy)
-                
+                cv2.putText(frame_copy, "WAIT! GETTING CURRENT BG", (200, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
+                cv2.imshow("Finger Count", frame_copy)
         else:
-            # now that we have the background, we can segment the hand.
-            
-            # segment the hand region
+            # Segment the hand region if we have the backgroud
             hand = segment(gray)
 
-            # First check if we were able to actually detect a hand
+            # First check if we were able to detect a hand
             if hand is not None:
-                
-                # unpack
                 thresholded, hand_segment = hand
-
                 # Draw contours around hand segment
                 cv2.drawContours(frame_copy, [hand_segment + (roi_right, roi_top)], -1, (255, 0, 0),1)
 
-                # Count the fingers
-                fingers = count_fingers(thresholded, hand_segment)
-
+                # Count the fingers and draw fingertip contours 
+                fingers, fingertip_contours = count_fingers(thresholded, hand_segment)
+                cv2.drawContours(frame_copy, fingertip_contours , -1, (0, 0, 255),1)
                 # Display count
-                cv2.putText(frame_copy, str(fingers), (70, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
-
-                # Also display the thresholded image
+                cv2.putText(frame_copy, str(fingers), (70, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+                # Display the thresholded image(WINDOW)
                 cv2.imshow("Thesholded", thresholded)
 
         # Draw ROI Rectangle on frame copy
-        cv2.rectangle(frame_copy, (roi_left, roi_top), (roi_right, roi_bottom), (0,0,255), 5)
-
-        # increment the number of frames for tracking
+        cv2.rectangle(frame_copy, (roi_left, roi_top), (roi_right, roi_bottom), (0,255,0), 5)
+        # Increment number of frames
         num_frames += 1
-
-        # Display the frame with segmented hand
+        # Display the unthresholded frame with segmented hand(WINDOW)
         cv2.imshow("Finger Count", frame_copy)
-
 
         # Close windows with Esc
         k = cv2.waitKey(1) & 0xFF
-
         if k == 27:
             break
 
